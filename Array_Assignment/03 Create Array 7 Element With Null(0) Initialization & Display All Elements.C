0b71c6b5c261730e0d63945b67e44ed360ee7ec1
RohanJadhav85 <136788747+RohanJadhav85@users.noreply.github.com>
2023-07-27 19:25:59 +0530

    Add files via upload

---
+++ Array_Assignment/03 Create Array 7 Element With Null(0) Initialization & Display All Elements.C
@@ -0,0 +1,21 @@
+// Create Array 7 Element With Null(0) Initialization & Display All Elements
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No[7] = {};
+
+    printf("\n Value of Element  1 = %d .",No[0]);
+    printf("\n Value of Element  2 = %d .",No[1]);
+    printf("\n Value of Element  3 = %d .",No[2]);
+    printf("\n Value of Element  4 = %d .",No[3]);
+    printf("\n Value of Element  5 = %d .",No[4]);
+    printf("\n Value of Element  6 = %d .",No[5]);
+    printf("\n Value of Element  7 = %d .",No[6]);
+
+    getch();
+    return 0;
+
+}
---
+++ Array_Assignment/04 Program to Accept & Display Elements Without Loop.C
@@ -0,0 +1,44 @@
+//  Program to Accept & Display Elements Without Loop
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int Arr[7];
+
+     printf("\n Enter Element 1 =>");
+     scanf("%d",&Arr[0]);
+
+     printf("\n Enter Element 2 =>");
+     scanf("%d",&Arr[1]);
+
+     printf("\n Enter Element 3 =>");
+     scanf("%d",&Arr[2]);
+
+     printf("\n Enter Element 4 =>");
+     scanf("%d",&Arr[3]);
+
+     printf("\n Enter Element 5 =>");
+     scanf("%d",&Arr[4]);
+
+     printf("\n Enter Element 6 =>");
+     scanf("%d",&Arr[5]);
+
+     printf("\n Enter Element 7 =>");
+     scanf("%d",&Arr[6]);
+
+     getch();
+
+     printf("\n Value of Element 101 is = %d",Arr[0]);
+     printf("\n Value of Element 102 is = %d",Arr[1]);
+     printf("\n Value of Element 103 is = %d",Arr[2]);
+     printf("\n Value of Element 104 is = %d",Arr[3]);
+     printf("\n Value of Element 105 is = %d",Arr[4]);
+     printf("\n Value of Element 106 is = %d",Arr[5]);
+     printf("\n Value of Element 107 is = %d",Arr[6]);
+
+     getch();
+     return 0;
+
+}

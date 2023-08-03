48f6cc7d97ea03b28e8f3a0917bb2b547c902362
RohanJadhav85 <136788747+RohanJadhav85@users.noreply.github.com>
2023-08-03 20:36:24 +0530

    Add files via upload

---
+++ Pattern_Print/02 Hollow Stars.C
@@ -0,0 +1,37 @@
+// Hollow Stars .C
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R = 0, C = 0, Cnt = 0;
+    printf("\n Enter a Number = ");
+    scanf("%d",&Cnt);
+
+    printf("\n ================***************================\n\n");
+
+    for(R = 1; R <= Cnt; R++)
+    {
+        for (C = 1; C <= Cnt; C++)
+        {
+            if (R == 1 || C == 1 || R == Cnt || C == Cnt)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n ================***************================\n\n");
+
+    getch();
+    return 0;
+
+
+}
+

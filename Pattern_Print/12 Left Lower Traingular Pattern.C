9d4955599ce61e475e54694a7732574d66f99b0d
RohanJadhav85 <136788747+RohanJadhav85@users.noreply.github.com>
2023-08-06 09:18:13 +0530

    Add files via upload

---
+++ Pattern_Print/12 Left Lower Traingular Pattern.C
@@ -0,0 +1,35 @@
+//  Left Lower Triangular Pattern .C
+
+#include<Stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R = 0, C = 0 , Cnt = 0;
+    printf("\n Enter a Number = ");
+    scanf("%d",&Cnt);
+
+    printf("\n ================***************================\n\n");
+
+    for(R = 1; R <= Cnt ;R++)
+    {
+        for(C = 1; C <= Cnt;C++)
+        {
+            if(R > C || R == C)  // (R >= C)
+            {
+                printf(" * ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+             printf("\n");
+
+    }
+    printf("\n ================***************================\n\n");
+
+     getch();
+     return 0;
+
+}

3537f843526cb2135f7bc1a1046d02d274a7defe
RohanJadhav85 <136788747+RohanJadhav85@users.noreply.github.com>
2023-08-14 18:09:31 +0530

    Add files via upload

---
+++ Bitwise/07 User Bitwise Right Shift.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#define size 2
+
+  int main()
+{
+  int Num[size] ={}, No =0 ,  Res = 0;
+
+   printf("\n Enter 1st Number =>");
+   scanf("%d",&No);
+
+
+   printf("\n===================********************====================\n");
+
+   Res = No >> size;
+
+   printf("\n Result => %d = %d",No,Res);
+
+   printf("\n\n===================********************====================\n");
+
+   getch();
+   return 0;
+
+
+}
+
+
